3b7ca04df57a5affeec35312887d04274e49cb58
AdityaTote <adityatote23@gmail.com>
2025-10-29 01:31:37 +0530

    chore: project change issue

--- apps/server/src/types/db.ts
+++
@@ -1,9 +0,0 @@
-import type { LibSQLDatabase } from "drizzle-orm/libsql";
-import * as schema from "@/lib/db/schema";
-
-export interface IUserContext {
-	id: number;
-	email: string;
-}
-
-export type DbType = LibSQLDatabase<typeof schema>;
