39f7dc42f646e42143497b4c53a170123eb69276
AdityaTote <adityatote23@gmail.com>
2025-10-29 01:18:26 +0530

    chore: fix minor bugss

--- apps/server/src/types/bindings.ts
+++ apps/server/src/types/bindings.ts
@@ -1,8 +1,6 @@
 import { DbType } from "./db";
 
 export interface Env {
-	TURSO_DATABASE_URL: string;
-	TURSO_AUTH_TOKEN: string;
 	FRONTEND_URL: string;
 	REDIS_URL: string;
 	REDIS_TOKEN: string;
